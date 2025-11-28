512cd7b9273188e9b18e9e0571b73caedf08fe33
Nikhil Nambiar Chengat <nikhilnambiar1212@gmail.com>
2025-11-27 21:48:06 -0500

    Create helloworld.go
    basic syntax, print statment, variables and comments

---
+++ helloworld.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// This is a single line comment
+	/* Multi
+	Line
+	Comment */
+	var name string = "Nikhil"
+	
+	fmt.Println("Hello, " + name + "!")
+}
